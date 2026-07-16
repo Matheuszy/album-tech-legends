831e1beedfcead762710a9c8ad748abb0959d12f
Matheuszy <matheusalmeidacarlos@gmail.com>
2026-07-16 17:55:17 -0300

    feat: add figurinhas endpoint and static image serving

--- back-end/main.py
+++ back-end/main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import os
 
-app = FastAPI();
+app = FastAPI()
 
-@app.get("/")
-def hello_world():
-    return {"menssage": "Hello world"}
+PASTA_BASE = os.path.dirname(os.path.abspath(__file__))
+PASTA_IMAGENS = os.path.join(PASTA_BASE, "figurinhas")
+
+app.mount("/imgs", StaticFiles(directory=PASTA_IMAGENS), name="imgs")
+
+figurinhas = [
+    {
+        "id": 1,
+        "nome": "Alan Turing",
+        "categoria": "IA",
+        "imagem_url": "/imgs/01-alan-turing.jpg"
+    },
+    {
+        "id": 2,
+        "nome": "John McCarthy",
+        "categoria": "IA",
+        "imagem_url": "/imgs/02-john-mccarthy.jpg"
+    },
+]
+
+
+@app.get("/figurinhas")
+def listar_figurinhas():
+    return figurinhas
